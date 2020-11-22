eef81bfdf09433e37e825c998b7f3a27bd68d3ca
fahmi.aldalou <fahmi.aldalou@gmail.com>
2020-11-22 14:46:25 +0200

    edit app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import Materials from './components/Materials';
-// import Mater from './components/Mater';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-
-    {/* <Mater /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
